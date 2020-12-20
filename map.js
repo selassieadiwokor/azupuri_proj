c17f8f88e2dbbc58199fbd6c79c5da38eaac2d85
myLORD <selassieadiwokor@gmail.com>
2020-12-20 16:17:32 -0600

    Update map.js

--- map.js
+++ map.js
@@ -1,5 +1,7 @@
-var mymap = L.map('mapid').setView([37.9692566,-91.7908471,], 8);
+	//map initialization
+	var mymap = L.map('mapid').setView([37.9692566,-91.7908471,], 8);
 
+	//map
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 		maxZoom: 18,
 		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -9,9 +11,8 @@ var mymap = L.map('mapid').setView([37.9692566,-91.7908471,], 8);
 		zoomOffset: -1
 	}).addTo(mymap);
 
-
-
-		for (var i = 0; i < myData.length; i++) {
-			//console.log(myData[i][0]);
-		  L.marker([myData[i][0], myData[i][1]]).addTo(mymap).bindPopup(myData[i][2]);;
-		}
+	//loop to plot
+	for (var i = 0; i < myData.length; i++) {
+		//console.log(myData[i][0]);
+	  L.marker([myData[i][0], myData[i][1]]).addTo(mymap).bindPopup(myData[i][2]);;
+	}
